03fd8ea7bb2be85b677feb428ae9ef02db46cccf
Izkipit <73563641+Izkipit@users.noreply.github.com>
2026-02-27 20:46:45 +0000

    Commit

--- DoublyLinkedList.java
+++ DoublyLinkedList.java
@@ -27,7 +27,17 @@ public class DoublyLinkedList<T> implements List<T> {
 	@Override
 	public void addFirst(T item) {
 		// TODO 
-		
+		Node newNode = new Node(item);
+
+        if (isEmpty()) {
+            head = tail = newNode;
+        } else {
+            newNode.next = head;
+            head.previous = newNode;
+            head = newNode;
+        }
+
+        numberOfElements++;
 			
 	}
 
@@ -50,32 +60,79 @@ public class DoublyLinkedList<T> implements List<T> {
 	@Override
 	public void print() {
 		// TODO
-		System.out.println("Hello");
+		Node current = head;
+        while (current != null) {
+            System.out.print(current.data + " ");
+            current = current.next;
+        }
+        System.out.println();
 	}
 
 	@Override
 	public void printBackwards() {
 		// TODO 
-			
+		Node current = tail;
+        while (current != null) {
+            System.out.print(current.data + " ");
+            current = current.previous;
+        }
+        System.out.println();
 	}
 
 	@Override
 	public boolean remove(T item) {
 		// TODO 
-	
-		return false;
+	if (isEmpty()) {
+            return false;
+        }
+
+        Node current = head;
+
+        while (current != null) {
+
+            if ((current.data == null && item == null) ||
+                (current.data != null && current.data.equals(item))) {
+
+                // Remove head
+                if (current == head) {
+                    head = head.next;
+                    if (head != null) {
+                        head.previous = null;
+                    } else {
+                        tail = null;
+                    }
+                }
+                // Remove tail
+                else if (current == tail) {
+                    tail = tail.previous;
+                    tail.next = null;
+                }
+                // Remove middle
+                else {
+                    current.previous.next = current.next;
+                    current.next.previous = current.previous;
+                }
+
+                numberOfElements--;
+                return true;
+            }
+
+            current = current.next;
+        }
+
+        return false;
 	}
 
 	@Override
 	public boolean isEmpty() {
 		// TODO 
-		return true; 
+		return numberOfElements == 0;
 	}
 
 	@Override
 	public int getLength() {
 		// TODO 
-		return 0;
+		return numberOfElements;
 	}
 	
 	/** 
